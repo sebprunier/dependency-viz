cd0e899a53c1a2efd95145e8732b1677f6941a2e
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2012-05-23 10:12:19 +0200

    Do not use settings.xml anymore and activate resource filtering

--- src/main/java/fr/jcreigno/depsviz/BooterContextListener.java
+++ src/main/java/fr/jcreigno/depsviz/BooterContextListener.java
@@ -1,14 +1,7 @@
 package fr.jcreigno.depsviz;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -16,176 +9,81 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.maven.repository.internal.DefaultServiceLocator;
 import org.apache.maven.repository.internal.MavenRepositorySystemSession;
-import org.apache.maven.settings.Mirror;
-import org.apache.maven.settings.Settings;
-import org.apache.maven.settings.building.DefaultSettingsBuilderFactory;
-import org.apache.maven.settings.building.DefaultSettingsBuildingRequest;
-import org.apache.maven.settings.building.DefaultSettingsProblem;
-import org.apache.maven.settings.building.SettingsBuildingException;
-import org.apache.maven.settings.building.SettingsProblem;
-import org.apache.maven.settings.building.SettingsProblem.Severity;
 import org.sonatype.aether.RepositorySystem;
 import org.sonatype.aether.RepositorySystemSession;
 import org.sonatype.aether.connector.file.FileRepositoryConnectorFactory;
 import org.sonatype.aether.connector.wagon.WagonProvider;
 import org.sonatype.aether.connector.wagon.WagonRepositoryConnectorFactory;
 import org.sonatype.aether.repository.LocalRepository;
-import org.sonatype.aether.repository.MirrorSelector;
 import org.sonatype.aether.repository.RemoteRepository;
 import org.sonatype.aether.spi.connector.RepositoryConnectorFactory;
 import org.sonatype.aether.util.DefaultRepositoryCache;
 import org.sonatype.aether.util.DefaultRepositorySystemSession;
-import org.sonatype.aether.util.repository.DefaultMirrorSelector;
+
+import com.google.common.collect.Lists;
 
 public class BooterContextListener implements ServletContextListener {
 
-	private ServletContext context = null;
+    private ServletContext context = null;
 
-	public void contextDestroyed(ServletContextEvent event) {
-		this.context = null;
+    public void contextDestroyed(ServletContextEvent event) {
+        this.context = null;
+    }
 
-	}
+    public void contextInitialized(ServletContextEvent event) {
+        this.context = event.getServletContext();
 
-	public void contextInitialized(ServletContextEvent event) {
-		this.context = event.getServletContext();
-		RepositorySystem sys = newRepositorySystem();
-		// setting repository system
-		this.context.setAttribute("RepositorySystem", sys);
-		Settings settings = null;
-		try {
-			settings = loadSettings(context.getInitParameter("user-settings"),
-					context.getInitParameter("global-settings"));
-		} catch (SettingsBuildingException e) {
-			this.context.log(e.getMessage(), e);
-		}
+        // setting repository system
+        RepositorySystem sys = newRepositorySystem();
+        this.context.setAttribute("repository-system", sys);
 
-		List<RemoteRepository> repos = new ArrayList<RemoteRepository>();
-		RemoteRepository central = new RemoteRepository("central", "default", getDefaultRemoteRepository());
-		this.context.log("using remote repo : " + central.getUrl());
-		repos.add(central);
-		if (context.getInitParameter("snapshots-remote-repo") != null) {
-			this.context.log("using snapshot remote repo : " + context.getInitParameter("snapshots-remote-repo"));
-			RemoteRepository snaps = new RemoteRepository("snapshot", "default",
-					context.getInitParameter("snapshots-remote-repo"));
-			snaps.setPolicy(true, null);
-			repos.add(snaps);
-		}
-		// setting remote repositories
-		this.context.setAttribute("repositories", repos);
-		// setting default session
-		this.context.setAttribute("session", newSystemSession(sys, settings));
-	}
+        // setting remote repositories
+        String remoteReposParam = context.getInitParameter("remote-repos");
+        List<RemoteRepository> remoteRepos = Lists.newArrayList();
 
-	private String getDefaultRemoteRepository() {
-		String init = context.getInitParameter("remote-repo");
-		return init == null ? "http://repo1.maven.org/maven2/" : init;
-	}
+        if (remoteReposParam == null) {
+            remoteRepos.add(new RemoteRepository("central", "default", "http://repo1.maven.org/maven2/"));
+            this.context.log("Using Maven Central by default : http://repo1.maven.org/maven2/");
+        } else {
+            String[] remoteReposUrls = remoteReposParam.split(";");
+            for (int i = 0; i < remoteReposUrls.length; i++) {
+                String repoUrl = remoteReposUrls[i];
+                remoteRepos.add(new RemoteRepository("repo" + i, "default", repoUrl));
+                this.context.log("Using remote repo : " + repoUrl);
+            }
+        }
 
-	public static RepositorySystem newRepositorySystem() {
-		/*
-		 * Aether's components implement org.sonatype.aether.spi.locator.Service
-		 * to ease manual wiring and using the prepopulated
-		 * DefaultServiceLocator, we only need to register the repository
-		 * connector factories.
-		 */
-		DefaultServiceLocator locator = new DefaultServiceLocator();
-		locator.addService(RepositoryConnectorFactory.class, FileRepositoryConnectorFactory.class);
-		locator.addService(RepositoryConnectorFactory.class, WagonRepositoryConnectorFactory.class);
-		locator.setServices(WagonProvider.class, new ManualWagonProvider());
+        this.context.setAttribute("repositories", remoteRepos);
 
-		return locator.getService(RepositorySystem.class);
-	}
+        // setting default session
+        this.context.setAttribute("session", newSystemSession(sys));
+    }
 
-	private Settings loadSettings(String user, String global) throws SettingsBuildingException {
-		DefaultSettingsBuildingRequest request = new DefaultSettingsBuildingRequest();
-		if (user != null) {
-			request.setUserSettingsFile(resolveFile(user));
-		}
-		if (global != null) {
-			request.setGlobalSettingsFile(resolveFile(global));
-		}
-		request.setSystemProperties(System.getProperties());
-		request.setUserProperties(new Properties());
-		Settings settings = new DefaultSettingsBuilderFactory().newInstance().build(request).getEffectiveSettings();
-		return settings;
-	}
+    private RepositorySystem newRepositorySystem() {
+        /*
+         * Aether's components implement org.sonatype.aether.spi.locator.Service to ease manual wiring and using the
+         * prepopulated DefaultServiceLocator, we only need to register the repository connector factories.
+         */
+        DefaultServiceLocator locator = new DefaultServiceLocator();
+        locator.addService(RepositoryConnectorFactory.class, FileRepositoryConnectorFactory.class);
+        locator.addService(RepositoryConnectorFactory.class, WagonRepositoryConnectorFactory.class);
+        locator.setServices(WagonProvider.class, new ManualWagonProvider());
 
+        return locator.getService(RepositorySystem.class);
+    }
 
-	/**
-	 * Try to load file from ServletContext.
-	 * 
-	 * @param filename
-	 *            file to load
-	 * @return corresonding file.
-	 * @throws SettingsBuildingException
-	 */
-	private File resolveFile(String filename) throws SettingsBuildingException {
-		File file = new File(this.context.getRealPath(filename));
-		return file.exists() ? file : new File(filename);
-	}
+    private RepositorySystemSession newSystemSession(RepositorySystem sys) {
+        DefaultRepositorySystemSession session = new MavenRepositorySystemSession();
 
-	/**
-	 * build problem list from exception.
-	 * 
-	 * @param file
-	 *            settings file.
-	 * @param e
-	 *            exception
-	 * @return problem list
-	 */
-	private List<SettingsProblem> explainProblems(String file, Exception e) {
-		List<SettingsProblem> pbs = new ArrayList<SettingsProblem>();
-		SettingsProblem pb = new DefaultSettingsProblem(e.getMessage(), Severity.ERROR, file, -1, -1, e);
-		pbs.add(pb);
-		return pbs;
-	}
+        // Local Repository
+        File localRepoPath = new File(new File(System.getProperty("user.home"), ".m2"), "repository");
+        LocalRepository localRepo = new LocalRepository(localRepoPath);
+        session.setLocalRepositoryManager(sys.newLocalRepositoryManager(localRepo));
 
-	public RepositorySystemSession newSystemSession(RepositorySystem sys, Settings settings) {
-		DefaultRepositorySystemSession session = new MavenRepositorySystemSession();
+        // Cache
+        session.setCache(new DefaultRepositoryCache());
 
-		Map<String, Object> configProps = new LinkedHashMap<String, Object>();
-		putAllEnvProperties(configProps);
-		session.setConfigProperties(configProps);
+        return session;
+    }
 
-		session.setOffline(settings.isOffline());
-		// session.setUserProperties(settings.getUserProperties());
-
-		LocalRepository localRepo = new LocalRepository(getDefaultLocalRepoDir(settings));
-		session.setLocalRepositoryManager(sys.newLocalRepositoryManager(localRepo));
-
-		// session.setProxySelector(getProxySelector());
-		session.setMirrorSelector(getMirrorSelector(settings));
-		// session.setAuthenticationSelector(getAuthSelector());
-
-		session.setCache(new DefaultRepositoryCache());
-
-		// session.setRepositoryListener(new AntRepositoryListener(task));
-
-		// session.setWorkspaceReader(ProjectWorkspaceReader.getInstance());
-
-		return session;
-	}
-
-	private MirrorSelector getMirrorSelector(Settings settings) {
-		DefaultMirrorSelector selector = new DefaultMirrorSelector();
-		for (Mirror mirror : settings.getMirrors()) {
-			selector.add(String.valueOf(mirror.getId()), mirror.getUrl(), mirror.getLayout(), false,
-					mirror.getMirrorOf(), mirror.getMirrorOfLayouts());
-		}
-
-		return selector;
-	}
-
-	private File getDefaultLocalRepoDir(Settings settings) {
-		if (settings.getLocalRepository() != null) {
-			return new File(settings.getLocalRepository());
-		}
-		return new File(new File(System.getProperty("user.home"), ".m2"), "repository");
-	}
-
-	private void putAllEnvProperties(Map<String, Object> configProps) {
-		for (Map.Entry<Object, Object> e : System.getProperties().entrySet()) {
-			configProps.put((String) e.getKey(), String.valueOf(e.getValue()));
-		}
-	}
 }
--- src/main/java/fr/jcreigno/depsviz/DependencyTreeHandler.java
+++ src/main/java/fr/jcreigno/depsviz/DependencyTreeHandler.java
@@ -38,98 +38,101 @@ import org.sonatype.aether.util.graph.selector.OptionalDependencySelector;
 import org.sonatype.aether.util.graph.selector.ScopeDependencySelector;
 import org.sonatype.aether.version.Version;
 
+import com.google.common.collect.Lists;
+
 @Path("/tree")
 @Produces({ MediaType.APPLICATION_JSON })
 public class DependencyTreeHandler {
 
-	private RepositorySystem system = null;
-	private RepositorySystemSession defaultSession = null;
-	private List<RemoteRepository> repositories = null;
-	private ServletContext context = null;
+    private RepositorySystem system = null;
+    private RepositorySystemSession defaultSession = null;
+    private List<RemoteRepository> repositories = null;
+    private ServletContext context = null;
 
-	public DependencyTreeHandler(@Context ServletContext ctx) {
-		context = ctx;
-		system = (RepositorySystem) context.getAttribute("RepositorySystem");
-		defaultSession = (RepositorySystemSession) context.getAttribute("session");
-		repositories = (List<RemoteRepository>) context.getAttribute("repositories");
-	}
+    public DependencyTreeHandler(@Context
+    ServletContext ctx) {
+        context = ctx;
+        system = (RepositorySystem) context.getAttribute("repository-system");
+        defaultSession = (RepositorySystemSession) context.getAttribute("session");
+        repositories = (List<RemoteRepository>) context.getAttribute("repositories");
+    }
 
-	@GET
-	@Path("{groupId}/{artifactId}")
-	public Response versions(@Context Request request, @PathParam("groupId") String groupId,
-			@PathParam("artifactId") String artifactId) {
-		RepositorySystemSession session = newRepositorySystemSession();
-		try {
-			Artifact artifact = new DefaultArtifact(groupId, artifactId, "pom", "[0.0.1,)");
-			VersionRangeRequest arequest = new VersionRangeRequest(artifact, repositories, null);
-			VersionRangeResult ars = system.resolveVersionRange(session, arequest);
-			List<Version> vs = ars.getVersions();
-			StringBuilder builder = new StringBuilder("[");
-			for (Version v : vs) {
-				builder.append('\'').append(v.toString()).append('\'');
-			}
-			builder.append(']');
-			return Response.ok(builder.toString()).build();
-		} catch (VersionRangeResolutionException e) {
-			return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).type("text/plain").build();
-		}
-	}
+    @GET
+    @Path("{groupId}/{artifactId}")
+    public Response versions(@Context
+    Request request, @PathParam("groupId")
+    String groupId, @PathParam("artifactId")
+    String artifactId) {
+        RepositorySystemSession session = newRepositorySystemSession();
+        try {
+            Artifact artifact = new DefaultArtifact(groupId, artifactId, "pom", "[0.0.1,)");
+            VersionRangeRequest arequest = new VersionRangeRequest(artifact, Lists.newArrayList(repositories), null);
+            VersionRangeResult ars = system.resolveVersionRange(session, arequest);
+            List<Version> vs = ars.getVersions();
+            StringBuilder builder = new StringBuilder("[");
+            for (Version v : vs) {
+                builder.append('\'').append(v.toString()).append('\'');
+            }
+            builder.append(']');
+            return Response.ok(builder.toString()).build();
+        } catch (VersionRangeResolutionException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).type("text/plain").build();
+        }
+    }
 
-	@GET
-	@Path("{groupId}/{artifactId}/{version}")
-	public Response list(@Context Request request, @PathParam("groupId") String groupId,
-			@PathParam("artifactId") String artifactId, @PathParam("version") String version) {
+    @GET
+    @Path("{groupId}/{artifactId}/{version}")
+    public Response list(@Context
+    Request request, @PathParam("groupId")
+    String groupId, @PathParam("artifactId")
+    String artifactId, @PathParam("version")
+    String version) {
 
-		Artifact artifact = new DefaultArtifact(groupId, artifactId, "pom", version);
-		boolean snapshot = artifact.isSnapshot();
+        Artifact artifact = new DefaultArtifact(groupId, artifactId, "pom", version);
+        boolean snapshot = artifact.isSnapshot();
 
-		// EntityTag etag = new EntityTag(name);
-		CacheControl cc = new CacheControl();
-		cc.setMaxAge(snapshot ? 180 : 3600);
-		// must revalidate snapshots
-		cc.setMustRevalidate(snapshot);
+        // EntityTag etag = new EntityTag(name);
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(snapshot ? 180 : 3600);
+        // must revalidate snapshots
+        cc.setMustRevalidate(snapshot);
 
-		RepositorySystemSession session = newRepositorySystemSession();
+        RepositorySystemSession session = newRepositorySystemSession();
 
-		try {
-			ArtifactRequest arequest = new ArtifactRequest(artifact, repositories, null);
-			ArtifactResult ars = system.resolveArtifact(session, arequest);
-			artifact = ars.getArtifact();
-		} catch (ArtifactResolutionException e) {
-			return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).type("text/plain").build();
-		}
+        try {
+            ArtifactRequest arequest = new ArtifactRequest(artifact, Lists.newArrayList(repositories), null);
+            ArtifactResult ars = system.resolveArtifact(session, arequest);
+            artifact = ars.getArtifact();
+        } catch (ArtifactResolutionException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).type("text/plain").build();
+        }
 
-		CollectRequest collectRequest = new CollectRequest(new Dependency(artifact, ""), repositories);
-		CollectResult collectResult = null;
-		try {
-			collectResult = system.collectDependencies(session, collectRequest);
-		} catch (DependencyCollectionException e) {
-			context.log(e.getMessage(), e);
-			throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-		} catch (IllegalArgumentException e) {
-			context.log(e.getMessage(), e);
-			throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-		}
-		JSonVisitor visitor = new JSonVisitor();
-		collectResult.getRoot().accept(new TreeDependencyVisitor(visitor));
-		return Response.ok(visitor.toString()).cacheControl(cc)
-		/* .tag(etag) */.build();
-	}
+        CollectRequest collectRequest = new CollectRequest(new Dependency(artifact, ""),
+                Lists.newArrayList(repositories));
+        CollectResult collectResult = null;
+        try {
+            collectResult = system.collectDependencies(session, collectRequest);
+        } catch (DependencyCollectionException e) {
+            context.log(e.getMessage(), e);
+            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
+        } catch (IllegalArgumentException e) {
+            context.log(e.getMessage(), e);
+            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
+        }
+        JSonVisitor visitor = new JSonVisitor();
+        collectResult.getRoot().accept(new TreeDependencyVisitor(visitor));
+        return Response.ok(visitor.toString()).cacheControl(cc)
+        /* .tag(etag) */.build();
+    }
 
-	private RepositorySystemSession newRepositorySystemSession() {
-		DefaultRepositorySystemSession session = new DefaultRepositorySystemSession(defaultSession);
+    private RepositorySystemSession newRepositorySystemSession() {
+        DefaultRepositorySystemSession session = new DefaultRepositorySystemSession(defaultSession);
 
-		DependencySelector depFilter = new AndDependencySelector(new ScopeDependencySelector("provided"),
-				new OptionalDependencySelector(), new ExclusionDependencySelector());
-		session.setDependencySelector(depFilter);
+        DependencySelector depFilter = new AndDependencySelector(new ScopeDependencySelector("provided"),
+                new OptionalDependencySelector(), new ExclusionDependencySelector());
+        session.setDependencySelector(depFilter);
 
-		// session.setTransferListener( new ConsoleTransferListener() );
-		// session.setRepositoryListener( new ConsoleRepositoryListener() );
-
-		// uncomment to generate dirty trees
-		// session.setDependencyGraphTransformer( null );
-
-		return session;
-	}
+        return session;
+    }
 
 }
